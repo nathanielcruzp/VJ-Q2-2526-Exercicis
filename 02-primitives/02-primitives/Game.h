eb8013885f739d7408645329ae90162b69860dbc
nathaniel.cruz <nathaniel.cruz@estudiantat.upc.edu>
2026-02-17 11:37:27 +0100

    02 círculos y cuadrados regulares. cambio viewport

--- 02-primitives/02-primitives/Game.h
+++ 02-primitives/02-primitives/Game.h
@@ -6,8 +6,8 @@
 #include "Scene.h"
 
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 800
 
 
 // Game is a singleton (a class with a single instance) that represents our whole application
